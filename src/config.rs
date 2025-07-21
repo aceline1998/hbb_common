750e2de6348e211e67178bbc9a7075d05e72521d
aceline1998 <13847497245@163.com>
2025-07-21 22:17:18 +0800

    修改IP和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.worklog.asia"];
+pub const RS_PUB_KEY: &str = "D2WBJ1RZj70Wl4s1hzRDu0HU2kqr1Sy3W5oiUeQ6Glg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
